aff8aa464a6591fd41374af49f8ef5ce7318d3ff
olwend <olwend@gmail.com>
2017-01-31 15:39:43 +0000

    Additional tests

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -25,4 +25,16 @@ describe("fizzBuzz", function() {
         });
     });
 
+    describe('fizzbuzz when number is not a factor of 3 and 5', function(){
+        it('displays number', function() {
+            expect(fizzBuzz.play(83)).toEqual(83);
+        });
+    });
+
+    describe('fizzbuzz when number is a decimal', function(){
+        it('fizzbuzzes for 3 and 5', function() {
+            expect(fizzBuzz.play(9.9)).toEqual(9.9);
+        });
+    });
+
 });
